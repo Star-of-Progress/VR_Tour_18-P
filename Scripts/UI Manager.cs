af19564be0ee79e771da7b0abde1bf93466850a8
Alimkulov Nurbol <124434364+VectorFayton@users.noreply.github.com>
2025-05-04 14:54:17 +0500

    Update UI Manager.cs

--- Scripts/UI Manager.cs
+++ Scripts/UI Manager.cs
@@ -10,10 +10,12 @@ public class UIManager : MonoBehaviour
     {
         
     }
-
+    // Changes the current scene to the specified scene ID
+    // Parameters:
+    //   scene_id: The build index of the scene to load (as defined in Build Settings)
     public void changeScene(int scene_id)
     {
-        SceneManager.LoadScene(scene_id);
+        SceneManager.LoadScene(scene_id); // Load the scene with the given build index
     }
     // Update is called once per frame
     void Update()
